11274b9869a1b22197c3998fdb5635e7c40fb051
SARAHXQuin <136741230+SARAHXQuin@users.noreply.github.com>
2023-08-08 22:39:32 +0530

    Update callback.py

--- ShizukaXMusic/plugins/admins/callback.py
+++ ShizukaXMusic/plugins/admins/callback.py
@@ -286,7 +286,7 @@ async def del_back_playlist(client, CallbackQuery, _):
                 db[chat_id][0]["markup"] = "tg"
             else:
                 button = stream_markup(_, videoid, chat_id)
-                img = await gen_thumb(videoid)
+                img = await gen_thumb(videoid, user_id)
                 run = await CallbackQuery.message.reply_photo(
                     photo=img,
                     caption=_["stream_1"].format(
