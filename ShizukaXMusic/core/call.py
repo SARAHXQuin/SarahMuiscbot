94232ece857794ebd229960101fefa3c1cfc1058
SARAHXQuin <136741230+SARAHXQuin@users.noreply.github.com>
2023-08-13 15:49:35 +0530

    Update call.py

--- ShizukaXMusic/core/call.py
+++ ShizukaXMusic/core/call.py
@@ -354,7 +354,7 @@ class Call(PyTgCalls):
                         original_chat_id,
                         text=_["call_9"],
                     )
-                img = await gen_thumb(videoid, user_id)
+                img = await gen_thumb(videoid, chat_id)
                 button = telegram_markup(_, chat_id)
                 run = await app.send_photo(
                     original_chat_id,
@@ -399,7 +399,7 @@ class Call(PyTgCalls):
                         original_chat_id,
                         text=_["call_9"],
                     )
-                img = await gen_thumb(videoid, user_id)
+                img = await gen_thumb(videoid, chat_id)
                 button = stream_markup(_, videoid, chat_id)
                 await mystic.delete()
                 run = await app.send_photo(
