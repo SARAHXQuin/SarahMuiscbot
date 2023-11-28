6000a03081af476c46282d7d92be77824bcf7c22
SmartGirl15 <92010030+SmartGirl15@users.noreply.github.com>
2023-11-28 13:40:25 +0530

    Update admins.py

--- ShizukaXMusic/utils/decorators/admins.py
+++ ShizukaXMusic/utils/decorators/admins.py
@@ -108,7 +108,7 @@ def AdminActual(mystic):
                 )
             except:
                 return
-            if not member.can_manage_voice_chats:
+            if not member.can_manage_video_chats:
                 return await message.reply(_["general_5"])
         return await mystic(client, message, _)
 
@@ -139,7 +139,7 @@ def ActualAdminCB(mystic):
                 )
             except:
                 return await CallbackQuery.answer(_["general_5"], show_alert=True)
-            if not a.can_manage_voice_chats:
+            if not a.can_manage_video_chats:
                 if CallbackQuery.from_user.id not in SUDOERS:
                     token = await int_to_alpha(CallbackQuery.from_user.id)
                     _check = await get_authuser_names(CallbackQuery.from_user.id)
